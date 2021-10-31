c0b3f7838d7154805edcb08a49747b7299a58ead
Taptej <taptej5008@gmail.com>
2021-10-31 18:35:27 +0530

    Contact Us Page

--- client/src/Components/FooterPages/Contact.js
+++ client/src/Components/FooterPages/Contact.js
@@ -1,7 +1,30 @@
+import classes from "./Layout.module.css" ;
+
 const Contact = ()=> {
     return (
         <div>
-          contact
+          <h2>Contact Us</h2>
+          <h5>The opinions of our users matter!</h5>
+          <div className={classes['detail-items']}>
+            <ul>
+              <li>
+                Have you found a bug?
+              </li>
+              <li>
+                Do you have suggestions?
+              </li>
+              <li>
+                Would like to give us a feedback?
+              </li>
+              <hr/>
+              <li>
+                <a href="mailto:xyz@gmail.com" className={classes.mail}>
+                 feedback@facegenerator.com
+                </a> 
+              </li>
+            </ul>
+            
+          </div>
         </div>
     ) ;
 } ;
