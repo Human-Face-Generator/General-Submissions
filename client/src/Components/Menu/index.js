374aed8e0514114b47e3a0776b1d7ece7a5ec2dd
Taptej <taptej5008@gmail.com>
2021-11-06 20:24:55 +0530

    navbar styled

--- client/src/Components/Menu/index.js
+++ client/src/Components/Menu/index.js
@@ -2,22 +2,29 @@ import React from "react";
 import { Nav,Navbar} from "react-bootstrap";
 import {Link} from 'react-router-dom';
 import './menu.css';
+import classes from './Menu.module.css' ;
+
 const Menu=()=>{
 
     return (
         <>
-        
-        <Navbar bg="light" expand="lg">
-        <Nav.Link as={Link}  className="HomeNav" to="/"> Home</Nav.Link>
+        <Navbar className={classes.bg}  expand="lg">
 
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link as={Link} to="/Uses">Use Cases</Nav.Link>
-        <Nav.Link as={Link} to="/account-images">Saved Images</Nav.Link>
-        <Nav.Link as={Link} to="/login">Login</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
+          <Nav.Link as={Link}  className={classes.home} to="/"> HOME </Nav.Link>
+          <Navbar.Toggle className={classes['toggle-icon']} aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+             <Nav.Link className={classes.link} as={Link} to="/Uses">
+              Use Cases
+             </Nav.Link>
+             <Nav.Link className={classes.link} as={Link} to="/account-images">
+              Saved Images
+             </Nav.Link>
+             <Nav.Link className={classes.link} as={Link} to="/login">
+              Login
+             </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
 
        </Navbar>
       
