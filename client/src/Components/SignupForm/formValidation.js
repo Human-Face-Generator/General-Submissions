11720070accf4573ed6eb20ebd9f65836e25ff83
rohitbajaj7877 <rohitbajaj7877@gmail.com>
2021-11-29 01:03:22 +0530

    password validator updated for sign-up page

--- client/src/Components/SignupForm/formValidation.js
+++ client/src/Components/SignupForm/formValidation.js
@@ -30,16 +30,21 @@ const checkEmail=(email)=>{
 }
 
 const checkPassword=({password,vpassword})=>{
-    if(!password)
-    {
-        errors.password="Please enter a password";
-    }
-    else if(password.length<6){
-        errors.password="Password should be atleast 6 characters long";
-    }
-    else if(password !== vpassword){
-        errors.vpassword="Passwords do not match";
-    }
+    var decimal= /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
+     if(!decimal.test(password))
+     {
+        errors.password="Password must be between 7 to 15 characters which contains at least one numeric digit and a special character";
+     }
+    // if(!password)
+    // {
+    //     errors.password="Please enter a password";
+    // }
+    // else if(password.length<6){
+    //     errors.password="Password should be atleast 6 characters long";
+    // }
+    // else if(password !== vpassword){
+    //     errors.vpassword="Passwords do not match";
+    // }
 }
 
 const checkFormInputs=({username,email,password,vpassword})=>{
